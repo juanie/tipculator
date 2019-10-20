8e4d31ac1d2763de368bdb4bd978c8ea5b0c8666
Juana <juanie@me.com>
2019-10-20 16:40:55 -0400

    deleted old files

--- script.js
+++ script.js
@@ -3,32 +3,32 @@ function calculateTip() {
   var tipPer = document.getElementById("tipPer").value;
   var split = document.getElementById("split").value;
 
- //validate input
- if (billTot === "" || serviceQual == 0) {
-  alert("Please enter values");
-  return;
+  //validate input
+  //  var tenPerBtn = document.querySelector('.tenPercent');
+  //  var fifteenPerBtn = document.querySelector('.fifteenPercent');
+  //  var twentyPerBtn = document.querySelector('.twentyPercent');
+  //  var customPerBtn = document.querySelector('.customTipBtn');
+  //  var bill = document.getElementById('billInput');
+  //  var tipSuggestion = document.getElementById('tipAmount');
+
+  if (billTot === "" || tipPer == 0) {
+    alert("Please enter values");
+    return;
+  }
+
+  const input = document.querySelector("billTot");
+
+  const input = document.querySelector("tipPer");
+
+  input.addEventListener("billTot");
+
+  if (billAmt === "" && percentageOfTip == 0) {
+    alert("Please enter values");
+  }
+  var input = document.getElementById("");
+  function percentage(num, per) {
+    return (num / 100) * per;
+  }
+  const service = document.getElementById("service").value;
+  console.log(percentage(1000, 20));
 }
-
-
-
-onst input = document.querySelector("billTot");
-
-
-const input = document.querySelector("tipPer");
-
-input.addEventListener("billTot");
-
-function updateValue(e) {
-  log.textContent = e.target.value;
-}
-
-// /validate input
-//if (billAmt === "" && percentageOfTip == 0) {
-// alert("Please enter values");
-// }
-// var input = document.getElementById("");
-// function percentage(num, per) {
-//   return (num / 100) * per;
-// }
-// const service = document.getElementById("service").value;
-// console.log(percentage(1000, 20))
