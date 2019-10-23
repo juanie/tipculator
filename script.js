30782c78d341c00debcb0158921e3e0628af479e
Juana <juanie@me.com>
2019-10-23 04:17:51 -0400

    updated files for tipolator and deleted files

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
+//Calculate Tip
+
 function calculateTip() {
-  var billTot = document.getElementById("billTot").value;
-  var tipPer = document.getElementById("tipPer").value;
-  var split = document.getElementById("split").value;
+    //getElementById() method returns the element value with specified ID
 
- //validate input
- if (billTot === "" || serviceQual == 0) {
-  alert("Please enter values");
-  return;
+    var billAmt = document.getElementById("billAmt").value;
+
+    var servicequality = document.getElementById("servicequality").value;
+    var billsplit = document.getElementById("billsplit").value;
+    var tipTotal = document.getElementById("tipTotal").value;
+    var result = document.getElementById("result").value;
+    var billTotal = document.getElementById("billTotal").value;
+
+    //Tip Calculation
+
+    var total = billAmt * servicequality; //For Tip Calculation
+    var Total = parseFloat(billAmt) + parseFloat(total); //For Total bill amount calculation
+    //Display  Tip
+
+    document.getElementById("tipTotal").innerHTML = total;
+
+    //Display Total
+
+    document.getElementById("billTotal").innerHTML = Total;
+
+    //calculate price per person
+    var resulted = (billAmt * servicequality) / billsplit;
+    document.getElementById("result").innerHTML = resulted;
 }
 
-
-
-onst input = document.querySelector("billTot");
-
-
-const input = document.querySelector("tipPer");
-
-input.addEventListener("billTot");
-
-function updateValue(e) {
-  log.textContent = e.target.value;
-}
-
-// /validate input
-//if (billAmt === "" && percentageOfTip == 0) {
-// alert("Please enter values");
-// }
-// var input = document.getElementById("");
-// function percentage(num, per) {
-//   return (num / 100) * per;
-// }
-// const service = document.getElementById("service").value;
-// console.log(percentage(1000, 20))
+//Click to call function
+document.getElementById("calculate").onclick = function() {
+    calculateTip();
+};
