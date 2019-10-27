d63bb28b1aded12559f1e88b300d561748d98cba
Juana <juanie@me.com>
2019-10-27 17:00:01 -0400

    reset changes

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 //Calculate Tip
 
 function calculateTip() {
-    //getElementById() method returns the element value with specified ID
-
     var billAmt = document.getElementById("billAmt").value;
+
     var servicequality = document.getElementById("servicequality").value;
     var billsplit = document.getElementById("billsplit").value;
-    var completeTotal = document.getElementById("completeTotal").value;
+    // var tipTotal = document.getElementById("tipTotal").value;
+    // var result = document.getElementById("result").value;
+    // var billTotal = document.getElementById("billTotal").value;
 
     //Tip Calculation
 
     var total = billAmt * servicequality; //For Tip Calculation
     var Total = parseFloat(billAmt) + parseFloat(total); //For Total bill amount calculation
-    Total = Total.toFixed(2);
-    total = total.toFixed(2);
-
     //Display  Tip
 
     document.getElementById("tipTotal").innerHTML = total;
@@ -23,14 +21,9 @@ function calculateTip() {
 
     document.getElementById("billTotal").innerHTML = Total;
 
-    //calculate tip per person
+    //calculate price per person
     var resulted = (billAmt * servicequality) / billsplit;
     document.getElementById("result").innerHTML = resulted;
-    resulted = resulted.toFixed(2);
-    //total bill
-    var completeTotal = billTotal + Total;
-    document.getElementById("completeTotal").innerHTML = completeTotal;
-    completeTotal = completeTotal.toFixed(2);
 }
 
 //Click to call function
